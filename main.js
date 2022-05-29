b2b3010a26942d44f554bdaf7029d563a2e5e380
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-05-30 02:43:28 +0800

    feat:删除账号功能 fix:不显示懒加载结果

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ Vue.use(uView)
 //只能在初始化uView完成之后才能加载store，因为这里用到了uni.$u.http
 import store from './store'
 
+// 初始化z-paging
+import zConfig from '@/uni_modules/z-paging/components/z-paging/js/z-paging-config'
+zConfig.setConfig(null)
+zConfig.setConfig({
+	'loading-more-title-custom-style':{
+		'font-size':'14px'
+	}
+})
+
 const app = new Vue({
     ...App,
 	store
--- store/modules/userManagement.js
+++ store/modules/userManagement.js
@@ -20,26 +20,27 @@ const mutations = {
 			prev[cur.account] = cur
 			return prev
 		}, {})
+		// debugger
 		// 更新密码
 		let userList = state.userList.reduce((prev, cur, index) => {
-			if (arr[cur.account] !== undefined) {
+			if (arr[cur.account] !== undefined && arr[cur.account].pwd !== '') {
+				// 更新
 				prev.push(arr[cur.account])
 				delete arr[cur.account]
-			} else
+			} else if (arr[cur.account] === undefined) {
+				// 新增
 				prev.push(cur)
+			} else {
+				// 密码置空就是删除
+				
+			}
 			return prev
 		}, [])
 		// debugger
 		let newAccount = Object.values(arr)
+		newAccount = newAccount.filter(e => e.pwd !== '')
+
 		userList = newAccount.concat(userList)
-		// arr.filter(e => e?.pwd !== undefined && e?.pwd !== '').forEach(e => {
-		// 	console.log('state.userList.splice', e)
-		// 	state.userList.splice(state.userList.length, 0, {
-		// 		account: e.account,
-		// 		pwd: e.pwd,
-		// 		name: e.name
-		// 	})
-		// })
 		state.userList = userList
 	},
 	SET_INIT_DONE(state) {
@@ -52,12 +53,17 @@ const actions = {
 	async appendAccount(context, arr) {
 		if (!(arr instanceof Array)) arr = [arr]
 		context.dispatch('__initOnce')
+		context.dispatch('__dispatch_set_account_with_storage',arr)
+		console.log('appendAccount.context.state.userList', arr, context.state.userList)
+	},
+	
+	__dispatch_set_account_with_storage(context,arr)
+	{
 		context.commit('SET_ACCOUNT', arr)
 		uni.setStorage({
 			data: context.state.userList,
 			key: context.state.__localStorageKey
 		})
-		console.log('appendAccount.context.state.userList', arr, context.state.userList)
 	},
 
 	__initOnce(context) {
@@ -67,6 +73,16 @@ const actions = {
 		if (list.length !== 0)
 			context.commit('SET_ACCOUNT', list)
 		context.commit('SET_INIT_DONE')
+	},
+
+	async deleteAccounts(context, accountList) {
+		let list = accountList.map(e => {
+			return {
+				account: e.account,
+				pwd: ''
+			}
+		})
+		context.dispatch('__dispatch_set_account_with_storage',list)
 	},
 
 	async clearAllAccount(context) {
