0a14155575c6e484e9b883e8137ccb3e60585327
theshdowaura <haojiejack@gmail.com>
2023-09-18 17:00:20 +0800

    构建了登录模块

---
+++ sign_module/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+)
+
+func main() {
+	// 创建cookie jar
+	jar, _ := cookiejar.New(nil)
+
+	// 创建http客户端
+	client := &http.Client{
+		Jar: jar,
+	}
+	USERNAME := ""
+	PASSWORD := ""
+	// 构造登录请求
+	loginUrl := "https://passport2.chaoxing.com/fanyalogin"
+	data := url.Values{}
+	data.Set("uname", USERNAME)
+	data.Set("password", PASSWORD)
+	data.Set("refer", "http://i.mooc.chaoxing.com")
+	data.Set("t", "true")
+	req, err := http.NewRequest("POST", loginUrl, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// 发送登录请求
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// 打印登录响应
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Login Response Body:", string(body))
+
+	// 打印cookies
+	fmt.Println("Cookies:", jar.Cookies(req.URL))
+}
