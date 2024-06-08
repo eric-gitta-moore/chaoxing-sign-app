6ae4581db70121e693ecaebc87fd915cfa14d3d4
Eric Moore <EricGittaMoore@duck.com>
2024-06-08 21:39:20 +0800

    chore(web): init client

--- projects/web/client/tailwind.config.ts
+++ projects/web/client/tailwind.config.ts
@@ -1,17 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "media",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
