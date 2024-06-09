4b7897e57f7b3abd8b1ae7f6e6e0c67aeda55a21
Eric Moore <EricGittaMoore@duck.com>
2024-06-09 14:22:53 +0800

    chore(native/mobile): init

--- projects/native/mobile/capacitor.config.ts
+++ projects/native/mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.github.eric.chaosign',
-  appName: '超星学习通签到',
+  appName: '学习通签到',
   webDir: 'www'
 };
 
