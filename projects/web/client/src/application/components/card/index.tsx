7219026e4c93ed100e81ec852dddea165f235b16
Eric Moore <EricGittaMoore@duck.com>
2024-06-10 12:20:01 +0800

    fix(web/client): update

--- projects/web/client/src/application/components/card/index.tsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames";
-
-export default function Card({ children, className }: { children: React.ReactElement[]; className: string }) {
-  return (
-    <div className={classNames("max-w-xl", className)}>
-      <div className="bg-white shadow-md rounded-b-xl dark:bg-black">{children}</div>
-    </div>
-  );
-}
--- projects/web/client/src/application/pages/my/index.tsx
+++
@@ -1,5 +0,0 @@
-import TabBarNavigator from "@/application/layout/tab-bar-navigator";
-
-export default function My() {
-  return <TabBarNavigator />;
-}
